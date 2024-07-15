9aa948e51fe268fac2da3870b9059a0cd2cb0fbe
Tess647 <thanni_taslihah@yahoo.com>
2024-07-15 16:12:44 +0100

    navbar updated

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,24 +1,18 @@
 // src/components/NavBar.js
-// Importing necessary libraries and components
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signOut } from '../store/authActions';
 import '../styles/NavBar.css';
 
-// Functional component for the Navigation Bar
 function NavBar() {
-  // useDispatch hook to dispatch actions
   const dispatch = useDispatch();
-  // useSelector hook to access the state
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
-  // Handler function for signing out
   const handleSignOut = () => {
     dispatch(signOut());
   };
 
-  // Render the navigation bar
   return (
     <nav className="navbar">
       <ul>
@@ -41,5 +35,4 @@ function NavBar() {
   );
 }
 
-// Exporting the NavBar component as the default export
 export default NavBar;
